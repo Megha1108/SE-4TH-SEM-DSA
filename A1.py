ab21c58f22f82cf9436ebb2ca1bb73c616110b65
Megha1108 <114331789+Megha1108@users.noreply.github.com>
2023-06-03 12:03:22 +0530

    Update A1.py

--- A1.py
+++ A1.py
@@ -1,4 +1,4 @@
-"Consider telephone book database of N clients. Make use of a hash table implementation to quickly look up client's telephone number. Make use of two collision handling techniques , compare them using number of comparisons required to find a set of telephone numbers"
+#Consider telephone book database of N clients. Make use of a hash table implementation to quickly look up client's telephone number. Make use of two collision handling techniques , compare them using number of comparisons required to find a set of telephone numbers
 
 def tele_database():
     phone_data = []
