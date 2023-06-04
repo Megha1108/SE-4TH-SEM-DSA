f2c296e17fe1fa3afaf8a271a57214c1397582cd
Megha1108 <114331789+Megha1108@users.noreply.github.com>
2023-06-04 08:46:19 +0530

    Update c13.cpp

--- c13.cpp
+++ c13.cpp
@@ -3,98 +3,88 @@ list to perform BFS. Use the map of the area around the college as the graph. Id
 prominent land marks as nodes and perform DFS and BFS on that.*/
 
 #include <iostream>
-#include <vector>
-#include <queue>
+#include <string.h>
+
 using namespace std;
-//----------------------------------------------------------------------------
-// Graph class
-class Graph {
-private:
-    int numVertices;
-    vector<vector<int>> adjacencyMatrix;
-    vector<vector<int>> adjacencyList;
-//----------------------------------------------------------------------------
-public:
-    Graph(int vertices) {
-        numVertices = vertices;
 
-        // Initialize adjacency matrix
-        adjacencyMatrix.resize(numVertices, vector<int>(numVertices, 0));
+const int MAXN = 100; // maximum number of nodes
 
-        // Initialize adjacency list
-        adjacencyList.resize(numVertices);
+int adj_matrix[MAXN][MAXN]; // adjacency matrix
+int adj_list[MAXN][MAXN]; // adjacency list
+int dist[MAXN]; // distance array for BFS
+bool visited[MAXN]; // visited array for DFS
+
+// DFS function
+void dfs(int node) {
+    visited[node] = true;
+    cout << node << " ";
+
+    for (int i = 0; i < MAXN; i++) {
+        if (adj_matrix[node][i] && !visited[i]) {
+            dfs(i);
+        }
     }
-//----------------------------------------------------------------------------
-    void addEdge(int src, int dest) {
-        adjacencyMatrix[src][dest] = 1;
-        adjacencyMatrix[dest][src] = 1;
+}
 
-        adjacencyList[src].push_back(dest);
-        adjacencyList[dest].push_back(src);
-    }
-//----------------------------------------------------------------------------
-    void DFS(int startVertex, vector<bool>& visited) {
-        visited[startVertex] = true;
-        cout << startVertex << " ";
+// BFS function
+void bfs(int node) {
+    memset(dist, -1, sizeof(dist));
+    dist[node] = 0;
 
-        for (int i = 0; i < numVertices; ++i) {
-            if (adjacencyMatrix[startVertex][i] == 1 && !visited[i]) {
-                DFS(i, visited);
+    int q[MAXN];
+    int front = 0, rear = 0;
+    q[rear++] = node;
+
+    while (front != rear) {
+        int u = q[front++];
+        cout << u << " ";
+
+        for (int i = 0; i < MAXN; i++) {
+            if (adj_list[u][i] && dist[i] == -1) {
+                dist[i] = dist[u] + 1;
+                q[rear++] = i;
             }
         }
     }
-//----------------------------------------------------------------------------
-    void DFS(int startVertex) {
-        vector<bool> visited(numVertices, false);
-        cout << "DFS Traversal: ";
-        DFS(startVertex, visited);
-        cout << endl;
-    }
-//----------------------------------------------------------------------------
-    void BFS(int startVertex) {
-        vector<bool> visited(numVertices, false);
-        queue<int> bfsQueue;
+}
 
-        visited[startVertex] = true;
-        bfsQueue.push(startVertex);
-
-        cout << "BFS Traversal: ";
-//----------------------------------------------------------------------------
-        while (!bfsQueue.empty()) {
-            int currVertex = bfsQueue.front();
-            bfsQueue.pop();
-            cout << currVertex << " ";
-
-            for (int i = 0; i < adjacencyList[currVertex].size(); ++i) {
-                int adjVertex = adjacencyList[currVertex][i];
-                if (!visited[adjVertex]) {
-                    visited[adjVertex] = true;
-                    bfsQueue.push(adjVertex);
-                }
-            }
-        }
-
-        cout << endl;
-    }
-};
-//----------------------------------------------------------------------------
 int main() {
-    // Create a graph
-    Graph graph(7);
+    int n, m; // number of nodes and edges
+    cout << "Enter the number of nodes: ";
+    cin >> n;
+    cout << "Enter the number of edges: ";
+    cin >> m;
 
-    // Add edges
-    graph.addEdge(0, 1);
-    graph.addEdge(0, 3);
-    graph.addEdge(1, 2);
-    graph.addEdge(1, 5);
-    graph.addEdge(2, 4);
-    graph.addEdge(2, 6);
+    // initialize adjacency matrix and adjacency list
+    for (int i = 0; i < MAXN; i++) {
+        for (int j = 0; j < MAXN; j++) {
+            adj_matrix[i][j] = 0;
+            adj_list[i][j] = 0;
+        }
+    }
 
-    // Perform DFS starting from vertex 0
-    graph.DFS(0);
+    // add edges to the adjacency matrix and adjacency list
+    cout << "Enter the edges: " << endl;
+    for (int i = 0; i < m; i++) {
+        int u, v, w;
+        cout << "Enter edge " << i+1 << " (u v w): ";
+        cin >> u >> v >> w;
+        adj_matrix[u][v] = adj_matrix[v][u] = w;
+        adj_list[u][v] = adj_list[v][u] = 1;
+    }
 
-    // Perform BFS starting from vertex 0
-    graph.BFS(0);
+    // perform DFS and BFS on the graph
+    int start_node;
+    cout << "Enter the starting node for DFS and BFS: ";
+    cin >> start_node;
+
+    cout << "DFS: ";
+    dfs(start_node);
+    cout << endl;
+
+    cout << "BFS: ";
+    bfs(start_node);
+    cout << endl;
 
     return 0;
 }
